14d0518408bb765afb8123499d556e4a5fe971a5
Marcos <kakoferrare87@gmail.com>
2019-06-27 20:45:44 -0300

    Componentes funcionais criados

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Rodape from '../common/template/rodape'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className='content-wrapper'>
+            <h1>Conteudo</h1>
+        </div>
+        <Rodape />
     </div>
 )
